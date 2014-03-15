c7e78921fe932501b79838d5612d12a2d5753ee5
Richard Chandler <softwarebear@hotmail.com>
2014-03-15 23:18:16 +0000

    smartAdmin.initializeation module runs

--- app/js/libs/smartAdmin.js
+++ app/js/libs/smartAdmin.js
@@ -1,17 +1,6 @@
 (function(){'use strict';
 
-
 angular
-.module("smartAdmin.pageSetup", [])
-.run(function(){
-
-    angular.element(document).on("ready", function() {
-        (function(){
-        })();
-    });
-});
-
-angular
-.module("smartAdmin", [/*"smartAdmin.initialization", "smartAdmin.pageSetup"*/])
+.module("smartAdmin", ["smartAdmin.initialization", "smartAdmin.pageSetup"])
 
 })();
