603b36402bd7efdf4b78eba2697e32bb72953b11
Richard Chandler <softwarebear@hotmail.com>
2014-03-15 22:08:07 +0000

    smartAdmin.initialization created

--- app/js/libs/smartAdmin.js
+++ app/js/libs/smartAdmin.js
@@ -1,15 +1,5 @@
 (function(){'use strict';
 
-angular
-.module("smartAdmin.initialization", [])
-.run(function(){
-
-    angular.element(document).on("ready", function() {
-        (function(){
-        })();
-    });
-});
-
 
 angular
 .module("smartAdmin.pageSetup", [])
